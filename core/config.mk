4b25ef97e2b560b96703850933373f653f05f5de
therootlord <igor_cestari@hotmail.com>
2019-05-13 00:45:11 +0000

    Revert "core: Include QCOM/MTK targets"
    This reverts commit a1c6c46af8a05a733606d8ec801e855786b5dc33.

--- core/config.mk
+++ core/config.mk
@@ -1106,10 +1106,4 @@ endif
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
 
-# Rules for QCOM targets
--include $(TOPDIR)vendor/sacred/build/core/qcom_target.mk
-
-# Rules for MTK targets
--include $(TOPDIR)vendor/sacred/build/core/mtk_target.mk
-
 include $(BUILD_SYSTEM)/dumpvar.mk
